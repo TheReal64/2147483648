59de752224c36ddc76adb5ac3f4f5554c25c5528
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-12-03 17:41:24 -0600

    Add moves counter

--- js/game_manager.js
+++ js/game_manager.js
@@ -28,6 +28,7 @@ GameManager.prototype.setup = function () {
     this.grid        = new Grid(previousState.grid.size,
                                 previousState.grid.cells); // Reload grid
     this.score       = previousState.score;
+  	this.moves       = previousState.moves;
     this.over        = previousState.over;
     this.won         = previousState.won;
     this.keepPlaying = previousState.keepPlaying;
@@ -35,6 +36,7 @@ GameManager.prototype.setup = function () {
   this.grid         = new Grid(this.size);
 
   this.score        = 0;
+	this.moves       = 0;
   this.over         = false;
   this.won          = false;
 
@@ -79,6 +81,7 @@ GameManager.prototype.actuate = function () {
   this.actuator.actuate(this.grid, {
     score:     this.score,
     over:      this.over,
+  	moves:		this.moves++,
     won:       this.won,
     bestScore: this.scoreManager.get()
   });
@@ -90,6 +93,7 @@ GameManager.prototype.serialize = function () {
   return {
     grid:        this.grid.serialize(),
     score:       this.score,
+  	moves:       this.moves,
     over:        this.over,
     won:         this.won,
     keepPlaying: this.keepPlaying
